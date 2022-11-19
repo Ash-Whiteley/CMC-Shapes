0242e8fa5ea4f030971b878883b3844800b3661d
Ashley Whiteley <awhiteley@protonmail.com>
2022-11-19 17:42:42 +0000

    Added comments to DisplayFactory methods

--- Factories/DisplayFactory.cs
+++ Factories/DisplayFactory.cs
@@ -7,11 +7,20 @@ namespace DiagramAnalysisTest.Factories
     /// </summary>
     public static class DisplayFactory
     {
+        /// <summary>
+        /// Factory method for creating a <see cref="AreaDisplay"/>. Will utilise <see cref="ShapesTest.Writers.Printer"/>
+        /// </summary>
+        /// <returns>A <see cref="AreaDisplay"/> instance.</returns>
         public static AreaDisplay CreateAreaDisplay()
         {
             return new AreaDisplay(WriterFactory.CreatePrinter());
         }
 
+        /// <summary>
+        /// Factory method for creating a <see cref="AreaDisplay"/> class for a provided <see cref="IWriter"/> implementation.
+        /// </summary>
+        /// <param name="writer">The <see cref="IWriter"/> implementation to be used.</param>
+        /// <returns>A <see cref="AreaDisplay"/> instance.</returns>
         public static AreaDisplay CreateAreaDisplay(IWriter writer)
         {
             return new AreaDisplay(writer);
