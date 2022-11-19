9dd2e808c90ce800feee65f9e914678fe559e888
Ashley Whiteley <awhiteley@protonmail.com>
2022-11-19 17:18:38 +0000

    Provided an implementation of ILoggableWriter: LoggablePrinter

---
+++ Classes/LoggablePrinter.cs
@@ -0,0 +1,29 @@
+﻿using DiagramAnalysisTest.Interfaces;
+using System.Collections.Generic;
+
+namespace DiagramAnalysisTest.Classes
+{
+    public class LoggablePrinter : Printer, ILoggableWriter
+    {
+        private readonly List<string> logs = new List<string>();
+
+        public List<string> Logs { get => logs; }
+
+        /// <summary>
+        /// Print a message to the console synchronously.
+        /// </summary>
+        /// <param name="message">A <see cref="string"/> message to display in the console.</param>
+        public override void Write(string message)
+        {
+            base.Write(message);
+
+            this.Log(message);
+        }
+
+        /// <inheritdoc/>
+        public void Log(string message)
+        {
+            this.logs.Add(message);
+        }
+    }
+}
