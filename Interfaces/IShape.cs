debab40611f06118d2ac080cab92fb58f627cdfe
Ashley Whiteley <awhiteley@protonmail.com>
2022-11-19 16:35:24 +0000

    Introduced shape interface for shape classes to implement

---
+++ Interfaces/IShape.cs
@@ -0,0 +1,10 @@
+﻿namespace DiagramAnalysisTest.Interfaces
+{
+    public interface IShape
+    {
+        /// <summary>
+        /// Gets a value indicating the <see cref="decimal"/> area of the shape.
+        /// </summary>
+        decimal Area { get; }
+    }
+}
