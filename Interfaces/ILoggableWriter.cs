33c78325101cf479686191cfbd610080f28e0231
Ashley Whiteley <awhiteley@protonmail.com>
2022-11-19 17:15:39 +0000

    Introduced ILoggableWriter, an interface for an IWriter that can log what it has written

---
+++ Interfaces/ILoggableWriter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace DiagramAnalysisTest.Interfaces
+{
+    public interface ILoggableWriter : IWriter
+    {
+        /// <summary>
+        /// A list of messages the writer previously logged.
+        /// </summary>
+        List<string> Logs { get; }
+
+        /// <summary>
+        /// Log the provided message.
+        /// </summary>
+        /// <param name="message">A <see cref="string"/> message to log.</param>
+        void Log(string message);
+    }
+}
